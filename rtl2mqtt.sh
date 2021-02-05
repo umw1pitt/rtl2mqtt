f0e109c1ddbb80599752ca5d68f5219cbcc67917
umw1pitt <50347500+umw1pitt@users.noreply.github.com>
2021-02-04 21:54:18 -0600

    Update rtl2mqtt.sh

--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -14,10 +14,16 @@ PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 #
 # Start the listener and enter an endless loop
 #
-/usr/local/bin/rtl_433 -F json |  while read line
-do
-  echo $line
 
-# Raw message to MQTT
-  echo $line  | /usr/bin/mosquitto_pub -h $MQTT_HOST -i RTL_433 -l -t "RTL_433/Raw"
+rtl_431 -f 344975000 -F json -M utc | while read line
+do
+  sensor_id=`echo $line |  jq -c ".id"`
+  sensor_data=`echo "$line"`
+  
+  echo $sensor_id
+  echo $sensor_data
+  #send sensor data to unique MQTT topic by sensor ID. MQTT debug on '-d'
+  echo $sensor_data | mosquitto_pub -h $MQTT_HOST -p 1881 -i RTL_433 -l -t honeywell/sensor/$sensor_id -u $USER -P $PASS -d
+
 done
+
